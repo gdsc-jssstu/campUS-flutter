cf738c8b064ee8dcdaddf837850b95c3ae1ee830
Syed Hisham Akmal <hisham0502@gmail.com>
2023-10-21 03:35:28 +0530

    Added elevated buttons corresponding to each screen in home screen

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -11,7 +11,50 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text('Home')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: Text('Login'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/signup');
+              },
+              child: Text('Signup'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/request');
+              },
+              child: Text('Request'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/myrequests');
+              },
+              child: Text('My Requests'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/myprofile');
+              },
+              child: Text('My Profile'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/aboutus');
+              },
+              child: Text('About Us'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
